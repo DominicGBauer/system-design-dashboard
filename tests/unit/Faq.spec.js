e50616b885ddf1b669879e43042864d06dc71114
Dom <dom@nonacreative.com>
2021-05-20 22:12:52 +0200

    test: add faq testing

---
+++ tests/unit/Faq.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import Faq from '@/components/Faq.vue'
+
+describe('Faq.vue', () => {
+  it('renders the Faq', () => {
+    const wrapper = shallowMount(Faq, {})
+    expect(wrapper.text()).toMatch('Questions and Answers')
+  })
+})
