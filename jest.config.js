6c6ffff9561bbffd60647dcac25fbc5487ebe2f0
Dom <dom@nonacreative.com>
2021-05-20 22:35:57 +0200

    fix: babel error

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest',
   },
   transformIgnorePatterns: ['/node_modules/(?!echarts|zrender)'],
 }
