55bb8d46ab2ae1ee9840a092e66cbfabe49f6dae
Ryan <mailforabe7@yahoo.com>
2021-05-21 09:33:33 +0200

    chore: updated text

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <h1>The Equity Risk Service at Your Fingertips</h1>
-    <p> This dashboard offers an easy way to access the ERS provided by The African Institute of Financial Markets and Risk Management. 
-    Let's get started! If this is your first time, here (insert hyperlink here) are the steps to use the dashboard. </p>
+    <p>
+      This dashboard offers an easy way to access the ERS provided by The
+      African Institute of Financial Markets and Risk Management. This service provides useful calculations and data on a variety of financial
+      instruments available in South Africa on the Johannesburg Stock Exchange. Let's get started! If this is your first time, here (insert hyperlink here) are the
+      steps to start using the dashboard.
+    </p>
   </div>
 </template>
 
