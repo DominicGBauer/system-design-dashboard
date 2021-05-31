52aa4c4f9c2dcc88689e8283ddd08b4b1e2eddfa
RationalRyan11 <80746660+RationalRyan11@users.noreply.github.com>
2021-05-31 16:44:18 +0200

    feat: add highlights (#15)
    * feat: inserted blank empty highlights
    
    * chore: improved highlights bar with dummy indices and data
    
    * chore: added testing to highlights
    
    * chore: styled highlights bar a bit
    
    * chore: a few touch ups
    
    * fix: used flexbox and neatened up
    
    * fix: added back testing
    
    * chore: removed blue background

---
+++ src/components/Highlights.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="highlights-bar">
+    <div class="betas">JSE Index:{{ jseIndex }}</div>
+    <div class="betas">{{ industryIndices[1] }}: {{ industryBetas[1] }}</div>
+    <div class="betas">{{ industryIndices[2] }}: {{ industryBetas[2] }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Highlights',
+  data() {
+    return {
+      industryBetas: ['0.7276 (5%)', '0.9 (-7%)', '1.1 (25%)', '0.68 (-30%)'],
+      industryIndices: [
+        'Oil and Gas',
+        'Chemicals',
+        'Mining',
+        'Forestry and Paper',
+      ],
+      jseIndex: '+15%',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.highlights-bar {
+  display: flex;
+}
+
+.betas {
+  flex: 1 1 auto;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+</style>
---
+++ tests/unit/Highlights.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import Highlights from '@/components/Highlights.vue'
+
+describe('Highlights.vue', () => {
+  it('renders the Highlights text', () => {
+    const wrapper = shallowMount(Highlights, {})
+    expect(wrapper.text()).toMatch(
+      'JSE Index:+15%Chemicals: 0.9 (-7%)Mining: 1.1 (25%)',
+    )
+  })
+})
