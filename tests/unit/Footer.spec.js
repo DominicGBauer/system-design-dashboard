aaac02745bf66bfd1e044cda6b60e6a58216bc99
Timothy Mwesigwa <ttusuubira@gmail.com>
2021-06-01 14:32:55 +0200

    chore: moretestschanges

--- tests/unit/Footer.spec.js
+++ tests/unit/Footer.spec.js
@@ -4,6 +4,8 @@ import Footer from '@/components/Footer.vue'
 describe('Footer.vue', () => {
   it('renders the Footer text', () => {
     const wrapper = shallowMount(Footer, {})
-    expect(wrapper.text()).toMatch('AFRICAN INSTITUTE OF FINANCIAL MARKETS AND RISK MANAGEMENTFaculty of CommerceUniversity of Cape Town © 2021 Copyright: AIFMRM')
+    expect(wrapper.text()).toMatch(
+      'AFRICAN INSTITUTE OF FINANCIAL MARKETS AND RISK MANAGEMENTFaculty of CommerceUniversity of Cape Town © 2021 Copyright: AIFMRM',
+    )
   })
 })
