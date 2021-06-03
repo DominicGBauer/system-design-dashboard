ac26b474551eb430cb474ccc478f4d5edf389bd5
Ryan Davies <mailforabe7@yahoo.com>
2021-06-03 11:32:26 +0200

    feat: added all of request to page

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="highlights-bar">
-    <div class="betas">JSE Index:{{ jseIndex }}</div>
+    <div class="betas">JSE Index:{{ jse1 }}</div>
     <div class="betas">{{ industryIndices[1] }}: {{ industryBetas[1] }}</div>
     <div class="betas">{{ industryIndices[2] }}: {{ industryBetas[2] }}</div>
   </div>
+  <div>{{ info }}</div>
 </template>
 
 <script>
+//Axios stuff
+import axios from 'axios'
+
+var jse11 = 5
+
 export default {
   name: 'Highlights',
   data() {
@@ -19,7 +25,12 @@ export default {
         'Forestry and Paper',
       ],
       jseIndex: '+15%',
+
+      jse1: jse11,
     }
+  },
+  mounted() {
+    axios.get('/api').then((response) => (this.info = response.data))
   },
 }
 </script>
