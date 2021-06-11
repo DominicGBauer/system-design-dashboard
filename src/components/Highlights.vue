d1743021137aa63fb71c9892708d283c468bfaea
RationalRyan11 <80746660+RationalRyan11@users.noreply.github.com>
2021-06-11 15:19:11 +0200

    feat: link highlights with backend (#21)
    * feat: added all of request to page
    
    * fix: step back to go forwards
    
    * feat: added coloring and finished all highlights
    
    * chore: refactor code
    
    * fix: finished refactoring code
    
    Co-authored-by: Dom <dom@nonacreative.com>

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -1,25 +1,174 @@
 <template>
   <div class="highlights-bar">
-    <div class="betas">JSE Index:{{ jseIndex }}</div>
-    <div class="betas">{{ industryIndices[1] }}: {{ industryBetas[1] }}</div>
-    <div class="betas">{{ industryIndices[2] }}: {{ industryBetas[2] }}</div>
+    <div class="indices">
+      All Share Index (QTR): {{ allShare.value }}
+      <span
+        :class="{
+          positive: allShare.change >= 0,
+          negative: !(allShare.change >= 0),
+        }"
+      >
+        ({{ allShare.change }}%)
+      </span>
+    </div>
+
+    <div class="indices">
+      Top 40 Index (QTR): {{ top40.value }}
+      <span
+        :class="{
+          positive: top40.change >= 0,
+          negative: !(top40.change >= 0),
+        }"
+      >
+        ({{ top40.change }}%)
+      </span>
+    </div>
+
+    <div class="indices">
+      F&I Index (QTR): {{ fAndI.value }}
+      <span
+        :class="{
+          positive: fAndI.change >= 0,
+          negative: !(fAndI.change >= 0),
+        }"
+      >
+        ({{ fAndI.change }}%)
+      </span>
+    </div>
+
+    <div class="indices">
+      Industrials Index (QTR): {{ industrials.value }}
+      <span
+        :class="{
+          positive: industrials.change >= 0,
+          negative: !(industrials.change >= 0),
+        }"
+      >
+        ({{ industrials.change }}%)
+      </span>
+    </div>
+
+    <div class="indices">
+      Resources Index (QTR): {{ resources.value }}
+      <span
+        :class="{
+          positive: resources.change >= 0,
+          negative: !(resources.change >= 0),
+        }"
+        v-on:mount="resources.isNegative = resources.change <= 0"
+      >
+        ({{ resources.change }}%)
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
+//Axios stuff
+import axios from 'axios'
+
 export default {
   name: 'Highlights',
   data() {
     return {
-      industryBetas: ['0.7276 (5%)', '0.9 (-7%)', '1.1 (25%)', '0.68 (-30%)'],
-      industryIndices: [
-        'Oil and Gas',
-        'Chemicals',
-        'Mining',
-        'Forestry and Paper',
-      ],
-      jseIndex: '+15%',
+      period: 66,
+
+      allShare: {
+        value: 0,
+        change: 0,
+        isNegative: false,
+      },
+
+      top40: {
+        value: 0,
+        change: 0,
+        isNegative: false,
+      },
+
+      fAndI: {
+        value: 0,
+        change: 0,
+        isNegative: false,
+      },
+
+      industrials: {
+        value: 0,
+        change: 0,
+        isNegative: false,
+      },
+
+      resources: {
+        value: 0,
+        change: 0,
+        isNegative: false,
+      },
     }
+  },
+  mounted() {
+    axios.get('/api/shares?share=J203').then(
+      (response) =>
+        (this.allShare = {
+          value: response.data[response.data.length - 1].value.toFixed(0),
+          change: (
+            (response.data[response.data.length - 1].value /
+              response.data[response.data.length - this.period].value -
+              1) *
+            100
+          ).toFixed(0),
+        }),
+    )
+
+    axios.get('/api/shares?share=J200').then(
+      (response) =>
+        (this.top40 = {
+          value: response.data[response.data.length - 1].value.toFixed(0),
+          change: (
+            (response.data[response.data.length - 1].value /
+              response.data[response.data.length - this.period].value -
+              1) *
+            100
+          ).toFixed(0),
+        }),
+    )
+
+    axios.get('/api/shares?share=J250').then(
+      (response) =>
+        (this.fAndI = {
+          value: response.data[response.data.length - 1].value.toFixed(0),
+          change: (
+            (response.data[response.data.length - 1].value /
+              response.data[response.data.length - this.period].value -
+              1) *
+            100
+          ).toFixed(0),
+        }),
+    )
+
+    axios.get('/api/shares?share=J257').then(
+      (response) =>
+        (this.industrials = {
+          value: response.data[response.data.length - 1].value.toFixed(0),
+          change: (
+            (response.data[response.data.length - 1].value /
+              response.data[response.data.length - this.period].value -
+              1) *
+            100
+          ).toFixed(0),
+        }),
+    )
+
+    axios.get('/api/shares?share=J258').then(
+      (response) =>
+        (this.resources = {
+          value: response.data[response.data.length - 1].value.toFixed(0),
+          change: (
+            (response.data[response.data.length - 1].value /
+              response.data[response.data.length - this.period].value -
+              1) *
+            100
+          ).toFixed(0),
+        }),
+    )
   },
 }
 </script>
@@ -29,9 +178,18 @@ export default {
   display: flex;
 }
 
-.betas {
+.indices {
   flex: 1 1 auto;
   text-transform: uppercase;
   font-weight: bold;
+  font-size: 13px;
+}
+
+.positive {
+  color: green;
+}
+
+.negative {
+  color: red;
 }
 </style>
