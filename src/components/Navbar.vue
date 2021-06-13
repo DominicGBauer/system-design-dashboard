d6723a00dba676189dbd7f9de9920fdbaf1f460f
Timothy Mwesigwa <ttusuubira@gmail.com>
2021-06-13 17:11:31 +0200

    feat: 127_Add_NavBar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <ul class="container">
+      <h6><a href="#Highlights">HIGHLIGHTS</a></h6>
+      <h6><a href="#PieChart">VISUALISATION</a></h6>
+      <h6><a href="#Faq">FAQ</a></h6>
+      <h6><a href="#WorkedExample">WORKED EXAMPLE</a></h6>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  background-color: #2c3e50;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: right;
+  padding: 2rem;
+
+  p {
+    margin: 5px;
+  }
+
+  a {
+    color: rgb(255, 255, 255);
+  }
+}
+</style>
