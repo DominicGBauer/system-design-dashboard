c7bab6c8c98f0f8f0fd7d69ae4809d551cb3166a
Dom <dom@nonacreative.com>
2021-06-14 13:31:08 +0200

    fix: id for example

--- src/components/WorkedExample.vue
+++ src/components/WorkedExample.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="#example">
+  <div class="example">
     <h1>Worked Example</h1>
     <paragraph
       text="In this worked example, assume that you have a portfolio consisting of
