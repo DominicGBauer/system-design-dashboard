2abe135afe1fbb1d422a86afa262b42a493199ee
Ryan Davies <mailforabe7@yahoo.com>
2021-06-25 09:43:38 +0200

    chore: added text and testing

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@ export default {
     PieCharts,
     SyntheticTable,
     Faq,
-    WorkedExample,   
+    WorkedExample,
     Footer,
   },
 }
--- src/components/SyntheticTable.vue
+++ src/components/SyntheticTable.vue
@@ -1,16 +1,18 @@
 <template>
-  <div> 
-    <h1> Synthetic Table </h1>
-    <p> Text introducing table. </p>
-
+  <div class="container">
+    <h1>Synthetic Table</h1>
+    <p>
+      This table goes beyond the composite risk statistics of the report and shows a synthetic construction of the most important risk statistics for an index based on its constituents. 
+      These statistics are then aggregated as per the constituents weight in the index. Below is an example of such a synthethic table using the All Share Index: 
+    </p>
   </div>
-  
-  
+
+
+
   <div class="table-container">
     <table v-if="data">
-
       <caption>
-          Table 1: Synthetic Table
+        Table 1: Synthetic Table
       </caption>
       <thead>
         <tr>
@@ -22,7 +24,7 @@
       </thead>
 
       <tbody>
-        <tr v-for="entry in data" v-bind:key="entry">
+        <tr v-for="entry in data" :key="entry">
           <td>{{ entry.instrument }}</td>
           <td>{{ entry.beta.toFixed(2) }}</td>
           <td>{{ entry.market_volatility.toFixed(2) }}</td>
@@ -34,7 +36,7 @@
 </template>
 
 <script>
-//Axios stuff
+
 import axios from 'axios'
 
 export default {
@@ -55,6 +57,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 
 .table-container {
   display: flex;
@@ -82,6 +85,11 @@ caption {
   padding: 16px;
   font-weight: bold;
   caption-side: bottom;
+}
+
+p {
+  margin: 2rem 20rem;
+  text-align: left;
 }
 
 
